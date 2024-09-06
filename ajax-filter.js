3e33994dac1a82c68db229e059124b9cb74cdcfa
rjxmateo <popinggoodbusiness@gmail.com>
2024-09-06 17:03:00 +0800

    Update ajax-filter.js

--- ajax-filter.js
+++ ajax-filter.js
@@ -1,37 +1,58 @@
 jQuery(document).ready(function($) {
-    var allProductsHtml = $('.products-loop').html(); // Store all products' HTML
+    var allProductsHtml = $('.products-loop').html();
 
-    $('.color-checkbox').change(function() {
+
+    function updateParentCheckbox(modalId) {
+        var modal = $('#' + modalId);
+        var parentCheckboxId = modalId.replace('modal-', 'color-checkbox-');
+        var parentCheckbox = $('#' + parentCheckboxId);
+        
+        var isChecked = modal.find('.color-checkbox:checked').length > 0;
+        parentCheckbox.prop('checked', isChecked);
+    }
+
+
+    function getSelectedColors() {
         var selectedColors = [];
-
-        // Collect all selected colors
         $('.color-checkbox:checked').each(function() {
             selectedColors.push($(this).val());
         });
+        return selectedColors;
+    }
 
-        // Log the currently selected colors
-        console.log('Selected Colors:', selectedColors);
 
+    function filterProductsByColors(selectedColors) {
         if (selectedColors.length === 0) {
-            // Show all products if no colors are selected
-            $('.products-loop').html(allProductsHtml);
+            $('.products-loop').html(allProductsHtml); 
             return;
         }
 
-        // AJAX request if there are selected colors
         $.ajax({
             url: ajax_filter_params.ajax_url,
             type: 'POST',
             data: {
                 action: 'filter_by_brand_color',
-                colors: selectedColors, // Send the selected colors array
+                colors: selectedColors, 
             },
             success: function(response) {
-                $('.products-loop').html(response); // Replace the product list with filtered results
+                $('.products-loop').html(response); 
             },
             error: function(xhr, status, error) {
                 console.error('AJAX Error:', error);
             }
         });
+    }
+
+ 
+    $('.custom-modal .color-checkbox').change(function() {
+        var modalId = $(this).closest('.custom-modal').attr('id');
+        updateParentCheckbox(modalId);
+    });
+
+   
+    $('.color-checkbox').change(function() {
+        var selectedColors = getSelectedColors();
+        console.log('Selected Colors:', selectedColors);
+        filterProductsByColors(selectedColors);
     });
 });
